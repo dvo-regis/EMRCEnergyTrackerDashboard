00e96495d83f4c11cf3198b7263a57ca1212a1a9
OverdriveSZN <70327132+OverdriveSZN@users.noreply.github.com>
2023-08-22 07:43:00 +0100

    modified server.js installed TS dependecies

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+// MongoDB connection URI and client options
 const uri = "mongodb+srv://fortuneregis:Emrc2023@emrctracker.p0xgiv4.mongodb.net/?retryWrites=true&w=majority";
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -11,28 +11,30 @@ const client = new MongoClient(uri, {
   }
 });
 
-// Connect to MongoDB
 async function connectToDatabase() {
   try {
     await client.connect();
     console.log("Connected to MongoDB");
-    return client.db("DashboardData"); 
+    return client.db("DashboardData");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
   }
 }
 
-// Import your endpoint files
-const getDataRouter = require('./routes/getData');
-const postDataRouter = require('./routes/postData');
+  // Create the Express app here
+  const app = express();
 
-// Start the server
+// Start the server and set up endpoints
 async function startServer() {
   const db = await connectToDatabase();
 
-  // Use the endpoint routers
-  app.use('/api/data', getDataRouter);
-  app.use('/api/data', postDataRouter);
+  // Import your endpoint files
+  const getDataRouter = require('./routes/getData');
+  const postDataRouter = require('./routes/postData');
+
+    // Use the endpoint routers
+    app.use('/api/data', getDataRouter);
+    app.use('/api/data', postDataRouter);
 
   const PORT = 3000;
   app.listen(PORT, () => {
@@ -40,4 +42,5 @@ async function startServer() {
   });
 }
 
+// Call the function to start the server
 startServer().catch(console.error);
